f347bb8d46e1c94558305033a2a9d7074dbf1ff8
Levi Opunga <86954179+Levi-Opunga@users.noreply.github.com>
2023-03-09 23:05:39 +0300

    fixed mobile view padding issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: "375px",
-      // => @media (min-width: 640px) { ... }
 
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... },
-    },
 
     extend: {
       fontFamily: {
